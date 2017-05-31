f729188fd583e6d58f8d4c10751aa0a637f8b66d
hnyuanzijian <18738395879@163.com>
2017-05-31 20:34:56 +0800

    file

---
+++ src/com/yuanzijian/itcast/file/Demo2.java
@@ -0,0 +1,15 @@
+package com.yuanzijian.itcast.file;
+
+import java.io.File;
+
+public class Demo2 {
+	private void mian() {
+		// TODO Auto-generated method stub
+		File file = new File(".");
+		System.out.println("当前路径是: +" + file.getAbsolutePath());
+		
+		File file2 = new File("..\\..\\a.txt");
+		System.out.println("存在么" + file2.exists());
+		
+	}
+}
